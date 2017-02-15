58e0f669c8788bd2e59821ed99e115f38f89b6a9
chjkw <chjkw84@gmail.com>
2017-02-15 10:27:29 +0900

    DB connected

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import logo from './img/stand_up_logo.png';
 import './App.css';
 import Editor from './Editor'
+import FirebaseDao from './FirebaseDao'
+import config from './config'
 
 class App extends Component {
   constructor(){
     super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dao = new FirebaseDao(config);
+    this.submit = this.submit.bind(this);
   }
-  //버튼을 눌렀을 때 Action. 지금은 일단 콘솔 값을 확인만 함
-  handleSubmit(e){
-    console.log(this,e);
+
+  submit(article){
+    if(article){
+      let key = this.dao.newKey();
+      let updated = this.dao.update( key, article );
+      return updated;
+    }
   }
   //익명 사용자 여부
   isAnonymous(){
@@ -22,7 +29,10 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <Editor {...this}/>
+        <Editor handleSubmit={this.submit} isAnonymous={this.isAnonymous}/>
+         <ul>
+         {this.getArticles()}
+         </ul>
       </div>
     );
   }
---
+++ src/config.js
@@ -0,0 +1,12 @@
+var config = {
+   apiKey: "AIzaSyAjoYN-metgJ9m20tHFxK-P_qyWGOh-lBM",
+   authDomain: "startup-e5a81.firebaseapp.com",
+   databaseURL: "https://startup-e5a81.firebaseio.com",
+   storageBucket: "startup-e5a81.appspot.com",
+   messagingSenderId: "697414532989"
+ };
+ //firebase.initializeApp(config);
+
+export var embedlyKey = process.env.REACT_APP_EMBEDLY_KEY;
+
+export default config;
